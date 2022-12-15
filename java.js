1483dac9653f8b56d0cd54ed51236015b685dfc2
Adina Gabriela Iamandi <iamandi.adina95@gmail.com>
2022-12-15 16:11:06 +0100

    add java

--- java.js
+++ java.js
@@ -1,21 +1,19 @@
 const addClass = (event) => {
-    const selezionato = event.target;
-    selezionato.classList.add("selected");
+    const elementoSelezionato = event.target;
+    elementoSelezionato.classList.add("selected");
 };
 const gestisciKeyUpSuInput = (event) => {
-    if (event.key === "Enter") {
-        const valoreInput = document.getElementById("text-input").value;
-        if (valoreInput === "") {
-            alert(
-                "ATTENZIONE!! Prima di premere invio assicurarsi che il campo non sia vuoto"
-            );
-        } else {
-            const nuovoDiv = document.createElement("div");
-            nuovoDiv.classList.add("list-item");
-            nuovoDiv.innerText = valoreInput;
-            nuovoDiv.onclick = addClass;
-            document.getElementById("list-item").appendChild(nuovoDiv);
-            document.getElementById("text-input").value = "";
-        }
+    const valoreInput = document.getElementById("text-input").value;
+    if (valoreInput === "") {
+        alert(
+            "ATTENZIONE!! Prima di premere invio assicurarsi che il campo non sia vuoto"
+        );
+    } else {
+        const nuovoDiv = document.createElement("div");
+        nuovoDiv.classList.add("list-item");
+        nuovoDiv.innerText = valoreInput;
+        nuovoDiv.onclick = addClass;
+        document.getElementById("list-item").appendChild(nuovoDiv);
+        document.getElementById("text-input").value = "";
     }
 };
