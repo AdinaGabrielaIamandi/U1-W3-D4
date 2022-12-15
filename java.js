77c7d5dc4328c6b76abbad8c580645dc1ac4c073
Adina Gabriela Iamandi <iamandi.adina95@gmail.com>
2022-12-15 15:39:19 +0100

    add elements

--- java.js
+++ java.js
@@ -4,7 +4,7 @@ const addClass = (event) => {
 };
 const gestisciKeyUpSuInput = (event) => {
     if (event.key === "Enter") {
-        const valoreInput = document.getElementById("text-input");
+        const valoreInput = document.getElementById("text-input").value;
         if (valoreInput === "") {
             alert(
                 "ATTENZIONE!! Prima di premere invio assicurarsi che il campo non sia vuoto"
@@ -13,6 +13,9 @@ const gestisciKeyUpSuInput = (event) => {
             const nuovoDiv = document.createElement("div");
             nuovoDiv.classList.add("list-item");
             nuovoDiv.innerText = valoreInput;
+            nuovoDiv.onclick = addClass;
+            document.getElementById("list-item").appendChild(nuovoDiv);
+            document.getElementById("text-input").value = "";
         }
     }
 };
